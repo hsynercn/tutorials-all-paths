562a231c70bc7dd54351593eaf1fc6cbeb5cebb6
Huseyin Can Ercan <huscanercan@gmail.com>
2023-08-07 05:22:54 +0000

    Add transpose matrix java solution

---
+++ Interview Questions/AlgoExpert/1_Easy/E_Transpose_Matrix/Java_Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Program {
+  public int[][] transposeMatrix(int[][] matrix) {
+    // Write your code here.
+    int[][] result = new int[matrix[0].length][matrix.length];
+    for(int i = 0; i < matrix.length; i++) {
+      for(int j = 0; j < matrix[i].length; j++) {
+        result[j][i] = matrix[i][j];
+      }
+    }
+    return result;
+  }
+}
